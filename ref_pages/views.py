e996c0b980174653d52af3a63e0925ebe8c2a5f5
AlexeyLuch <pochtamoyapochta123@gmail.com>
2020-01-29 14:15:07 +0200

    added functions "categogy" and "support" to the views.py

--- ref_pages/views.py
+++ ref_pages/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Referer
+
+
+def category(request):
+    return render(request, 'ref_pages/category.html')
+
+
+def support(request):
+    contact_list = Referer.objects.all()
+    paginator = Paginator(contact_list, 25)  # Show 25 contacts per page
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'ref_pages/index.html', {'contacts': contacts})
