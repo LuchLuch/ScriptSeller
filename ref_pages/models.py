2bcbfefbd442560bbbd5fe5b8381a40af6f8fbe6
AlexeyLuch <pochtamoyapochta123@gmail.com>
2020-01-29 14:19:18 +0200

    Create new model "Referer"

--- ref_pages/models.py
+++ ref_pages/models.py
@@ -1,3 +1,20 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+
+class Referer(models.Model):
+
+    class Meta():
+        db_table = "ref_pages_referer"
+    ref_link = models.CharField(max_length=200)
+    describe_link = models.TextField()
+    free_test = models.BooleanField(default=True)
+    personal_proxy = models.BooleanField(default=True)
+    change_proxy = models.BooleanField(default=True)
+    public = models.BooleanField(default=True)
+    img = models.ImageField(upload_to="ref_img/",default='img/q12.jpg')
+
+
+    def __str__(self):
+        return self.describe_link
